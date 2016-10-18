eb3963ecc6018193c321ce5c9fd9cc42b39beed2
noobie <julian.dehm@fu-berlin.de>
2016-10-19 00:55:17 +0200

    add license info to file

--- layout_switcher.py
+++ layout_switcher.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+'''
+    Copyright (C) 2016 Julian Dehm
+
+    This file is part of "anki layout switcher".
+
+    "anki layout switcher" is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    "anki layout switcher" is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with "FOS Weather Widget".  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 
 from anki.hooks import addHook, wrap
 import re
